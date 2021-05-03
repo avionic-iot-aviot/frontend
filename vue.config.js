9e32f2b7a79aa96edb86f7a7ee4bdc009446be92
DESKTOP-BVE7QND\asant <asantangelo90@gmail.com>
2021-05-03 12:45:28 +0200

    Removed publicPath.

--- vue.config.js
+++ vue.config.js
@@ -9,7 +9,7 @@ module.exports = {
       }
     }
   },
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/production-sub-path/'
-    : '/'
+  // publicPath: process.env.NODE_ENV === 'production'
+  //   ? '/production-sub-path/'
+  //   : '/'
 }
