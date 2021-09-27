d5823ba652ed13119286464650959b5a32c9a8a8
Marco Cavalli <cmarcos46@gmail.com>
2021-09-27 11:02:40 +0200

    Reverting commit on file main.js

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import store from './store'
 import vuetify from './plugins/vuetify';
 import i18n from "./i18n";
 import "./filters";
+import config from "./config.js"
 
 Vue.config.productionTip = false
 
