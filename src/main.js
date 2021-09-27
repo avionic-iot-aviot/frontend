22706de9f01d684c1732b3dbab0da7b6ee7f988d
Marco Cavalli <cmarcos46@gmail.com>
2021-09-27 11:00:14 +0200

    Reverting commit on file main.js

--- src/main.js
+++ src/main.js
@@ -33,7 +33,7 @@ extend('required', {
   computesRequired: true
 });
 
-const tenantId="protezionecivile";//config.tenantId ? config.tenantId : window.location.href.split("/")[2].split(".")[0];
+const tenantId=config.tenantId ? config.tenantId : window.location.href.split("/")[2].split(".")[0];
 Vue.prototype.$dbapp_url=window.location.protocol+"//"+tenantId+".aviot.it/dbapp";
 Vue.prototype.$wss_url=window.location.protocol+"//wss."+tenantId+".aviot.it";
 Vue.prototype.$controller_url=window.location.protocol+"//controller."+tenantId+".aviot.it";
